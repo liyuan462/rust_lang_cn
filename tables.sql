307ac0278179509fffd72f37c1240ce1edb030cb
liyuan <liyuan462@gmail.com>
2016-06-13 19:01:48 +0800

    change mysql character set from utf8 to utf8mb4

--- tables.sql
+++ tables.sql
@@ -1,20 +1,20 @@
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(32) NOT NULL,
-  `email` varchar(64) NOT NULL,
-  `password` varchar(32) NOT NULL,
-  `salt` varchar(32) NOT NULL,
+  `username` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `salt` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
   `create_time` datetime NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `article` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `category` tinyint(4) NOT NULL DEFAULT '0',
-  `title` varchar(64) NOT NULL,
-  `content` text NOT NULL,
+  `title` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `content` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `user_id` int(11) NOT NULL,
   `comments_count` int(11) NOT NULL DEFAULT '0',
   `create_time` datetime NOT NULL,
@@ -24,21 +24,21 @@ CREATE TABLE `article` (
   `update_time` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
-  FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  CONSTRAINT `article_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `comment` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `article_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `content` text NOT NULL,
+  `content` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `create_time` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `article_id` (`article_id`),
   KEY `user_id` (`user_id`),
-  FOREIGN KEY (`article_id`) REFERENCES `article` (`id`),
-  FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`article_id`) REFERENCES `article` (`id`),
+  CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `message` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -46,7 +46,7 @@ CREATE TABLE `message` (
   `comment_id` int(11) DEFAULT NULL,
   `from_user_id` int(11) DEFAULT NULL,
   `to_user_id` int(11) NOT NULL,
-  `content` text,
+  `content` mediumtext COLLATE utf8mb4_unicode_ci,
   `mode` tinyint(4) NOT NULL DEFAULT '0',
   `status` tinyint(4) NOT NULL DEFAULT '0',
   `create_time` datetime NOT NULL,
@@ -55,8 +55,8 @@ CREATE TABLE `message` (
   KEY `from` (`from_user_id`),
   KEY `to` (`to_user_id`),
   KEY `comment_id` (`comment_id`),
-  FOREIGN KEY (`article_id`) REFERENCES `article` (`id`),
-  FOREIGN KEY (`from_user_id`) REFERENCES `user` (`id`),
-  FOREIGN KEY (`to_user_id`) REFERENCES `user` (`id`),
-  FOREIGN KEY (`comment_id`) REFERENCES `comment` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  CONSTRAINT `message_ibfk_1` FOREIGN KEY (`article_id`) REFERENCES `article` (`id`),
+  CONSTRAINT `message_ibfk_2` FOREIGN KEY (`from_user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `message_ibfk_3` FOREIGN KEY (`to_user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `message_ibfk_4` FOREIGN KEY (`comment_id`) REFERENCES `comment` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
